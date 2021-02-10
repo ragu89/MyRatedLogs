62585d8429928f27d1b3ab454c69ff8cd990ba0b
Guye Raphael, I233 <raphael.guye@post.ch>
2021-02-10 16:04:17 +0100

    Initial Commit

---
+++ MyRatedLogs/MyRatedLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRatedLogsApp.swift
+//  MyRatedLogs
+//
+//  Created by Guye Raphael, I233 on 10.02.21.
+//
+
+import SwiftUI
+
+@main
+struct MyRatedLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
